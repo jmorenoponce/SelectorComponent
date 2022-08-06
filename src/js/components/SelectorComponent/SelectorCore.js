1befc63ece2f718c7934854cef9e30f90360c1ad
Jose Moreno <jose.moreno@avanzasi.es>
2022-08-06 11:09:51 +0200

    A vueltas con el refactor

--- src/js/components/SelectorComponent/SelectorCore.js
+++ src/js/components/SelectorComponent/SelectorCore.js
@@ -10,128 +10,133 @@ import $ from "jquery";
 export class SelectorCore {
 
 
-	constructor(managerId, instanceId) {
+    constructor(managerId, instanceId) {
 
-		this._managerId = managerId;
+        this._managerId = managerId;
 
-		this._instanceId = instanceId;
-		this._instanceName = null;
+        this._instanceId = instanceId;
+        this._instanceName = null;
 
         // Todo: Refactor, la config la lleva la interface
-		this._config = new SelectorConfig();
-		this._sourceHtmlObj = null;
-		this._interface = null; // Todo: Retorna State
+        this._config = new SelectorConfig();
+        this._sourceHtmlObj = null;
+        this._interface = null; // Todo: Retorna State
 
-		// Todo: Refactor, los states vienen de la interface ¿pero necesito estados aquí en Core?
-		this._state = this._config.STATES.WAITING_FOR_BINDING;
+        // Todo: Refactor, los states vienen de la interface ¿pero necesito estados aquí en Core?
+        this._state = this._config.STATES.WAITING_FOR_BINDING;
 
-		return this._state;
-	}
-
-
-	bind(component, configObj) {
-
-		let _tmpResponse = this._config.assignConfig(configObj);
-
-		if (typeof _tmpResponse === 'object') {
-
-			this._sourceHtmlObj = $(component); // Todo: Validar componente
-
-			let _tmpName = this._sourceHtmlObj.attr('data-selector-name').trim();
-			_tmpName ? this._instanceName = _tmpName : false;
-
-			this._state = this._config.STATES.BINDED;
-
-		} else {
-
-			this._state = _tmpResponse;
-		}
-
-		return this._state;
-	}
+        return this._state;
+    }
 
 
-	_init() {
+    bind(component, configObj) {
 
-		if (this._state === this._config.STATES.BINDED) {
+        let _tmpResponse = this._config.assignConfig(configObj);
 
-			// Todo: Refactor, instanciar la interface en el bind para darle config
-		    this._interface = new SelectorInterface(this._sourceHtmlObj, this._config.configObj);
-			this._state = this._config.STATES.RUNNING; // Todo: again
-		}
+        if (!$(component).length > 0) {
 
-		return this._state;
-	}
+        }
 
 
-	init() {
+        if (typeof _tmpResponse === 'object') {
 
-		return this._init();
-	}
+            this._sourceHtmlObj = $(component); // Todo: Validar componente
 
+            let _tmpName = this._sourceHtmlObj.attr('data-selector-name').trim();
+            _tmpName ? this._instanceName = _tmpName : false;
 
-	get id() {
+            this._state = this._config.STATES.BINDED;
 
-		return this._instanceId;
-	}
+        } else {
 
+            this._state = _tmpResponse;
+        }
 
-	get name() {
-
-		return this._instanceName;
-	}
-
-
-	get state() {
-
-		return this._state;
-	}
+        return this._state;
+    }
 
 
-	get parentManagerId() {
+    _init() {
 
-		return this._managerId;
-	}
+        if (this._state === this._config.STATES.BINDED) {
 
+            // Todo: Refactor, instancia la interface en el bind para darle config, y que la guarde ella
+            this._interface = new SelectorInterface(this._sourceHtmlObj, this._config.configObj);
+            this._state = this._config.STATES.RUNNING; // Todo: again
+        }
 
-	enable() {
-
-	}
-
-
-	disable() {
-
-	}
+        return this._state;
+    }
 
 
-	update() {
+    init() {
+
+        return this._init();
+    }
 
 
-	}
+    get id() {
+
+        return this._instanceId;
+    }
 
 
-	selectItems(target) {
+    get name() {
 
-	}
-
-
-	unselectItems(target) {
-
-	}
+        return this._instanceName;
+    }
 
 
-	clear() {
+    get state() {
 
-	}
-
-
-	destroy() {
-
-	}
+        return this._state;
+    }
 
 
-	_submitInterfaceError(code) {
+    get parentManagerId() {
 
-		// this._interface.error(code);
-	}
+        return this._managerId;
+    }
+
+
+    enable() {
+
+    }
+
+
+    disable() {
+
+    }
+
+
+    update() {
+
+
+    }
+
+
+    selectItems(target) {
+
+    }
+
+
+    unselectItems(target) {
+
+    }
+
+
+    clear() {
+
+    }
+
+
+    destroy() {
+
+    }
+
+
+    _submitInterfaceError(code) {
+
+        // this._interface.error(code);
+    }
 }
