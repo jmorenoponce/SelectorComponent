cdc7ac7a131fe1aca47776b01c893a669f0e51cf
Jose Moreno <jose.moreno@avanzasi.es>
2022-08-26 06:34:07 +0200

    Revisión Badge.js

--- src/js/badge.js
+++ src/js/badge.js
@@ -1,48 +1,48 @@
 $(function () {
 
-	$('body').on('click', '.ux-popover-trigger', function (e) {
-		e.stopImmediatePropagation();
-		UI_Popover.trigger_click(this, e);
-	});
+    $('body').on('click', '.ux-popover-trigger', function (e) {
+        e.stopImmediatePropagation();
+        UI_Popover.trigger_click(this, e);
+    });
 
-	$('body').on('click', function (e) {
-		UI_Popover.on_body_click(e);
-	});
+    $('body').on('click', function (e) {
+        UI_Popover.on_body_click(e);
+    });
 
 });
 
 
 function UI_Popover(id, trigger) {
 
-	this._id = id;
-	UI_Popover._instances[this._id] = this;
+    this._id = id;
+    UI_Popover._instances[this._id] = this;
 
-	this._trigger = trigger;
-	this._$popover = $(trigger).find('.ux-popover:first');
-	this._is_shown = this._$popover.attr('data-shown') === '1';
-	this._trigger.setAttribute('data-popover-id', id);
+    this._trigger = trigger;
+    this._$popover = $(trigger).find('.ux-popover:first');
+    this._is_shown = this._$popover.attr('data-shown') === '1';
+    this._trigger.setAttribute('data-popover-id', id);
 }
 
 
 UI_Popover.on_body_click = function (e) {
 
-	// Outer popover clicks will trigger the popover close
-	let $target = $(e.target);
+    // Outer popover clicks will trigger the popover close
+    let $target = $(e.target);
 
-	if (e.target.classList.contains('.ux-popover') || $target.closest('.ux-popover').length) {
-		// Prevent .ux-popover  clicks to hide the popover element
-		return;
-	}
+    if (e.target.classList.contains('.ux-popover') || $target.closest('.ux-popover').length) {
+        // Prevent .ux-popover  clicks to hide the popover element
+        return;
+    }
 
-	this.closeAll();
+    this.closeAll();
 };
 
 
 UI_Popover.closeAll = function () {
 
-	for (let instance of UI_Popover._instances) {
-		instance.close();
-	}
+    for (let instance of UI_Popover._instances) {
+        instance.close();
+    }
 };
 
 
@@ -50,80 +50,80 @@ UI_Popover._instances = [];
 
 UI_Popover.getInstanceById = function (id) {
 
-	return UI_Popover._instances[id] || false;
+    return UI_Popover._instances[id] || false;
 };
 
 
 UI_Popover.getTriggerInstance = function (trigger) {
 
-	let ins;
-	let id = trigger.getAttribute('data-popover-id');
+    let ins;
+    let id = trigger.getAttribute('data-popover-id');
 
-	if (!id) {
-		ins = new UI_Popover(UI_Popover._instances.length, trigger);
-	} else {
-		ins = UI_Popover.getInstanceById(parseInt(id));
-	}
+    if (!id) {
+        ins = new UI_Popover(UI_Popover._instances.length, trigger);
+    } else {
+        ins = UI_Popover.getInstanceById(parseInt(id));
+    }
 
-	return ins;
+    return ins;
 };
 
 
 UI_Popover.trigger_click = function (trigger, e) {
 
-	let popover = UI_Popover.getTriggerInstance(trigger);
+    let popover = UI_Popover.getTriggerInstance(trigger);
 
-	popover.trigger_click(e);
+    popover.trigger_click(e);
 }
 
 
 UI_Popover.prototype.close = function () {
 
-	this._hide();
+    this._hide();
 }
 
 
 UI_Popover.prototype.trigger_click = function (e) {
 
-	let $target = $(e.target);
+    let $target = $(e.target);
 
-	if (e.target.classList.contains('.ux-popover') || $target.closest('.ux-popover').length) {
-		// Prevent .ux-popover  clicks to hide the popover element
-		return;
-	}
+    if (e.target.classList.contains('.ux-popover') || $target.closest('.ux-popover').length) {
+        // Prevent .ux-popover  clicks to hide the popover element
+        return;
+    }
 
-	if (this.isShown()) {
+    if (this.isShown()) {
 
-		this._hide();
+        this._hide();
 
-	} else {
+    } else {
 
-		this._show();
-	}
+        this._show();
+    }
 };
 
 
 UI_Popover.prototype.isShown = function () {
 
-	return this._is_shown;
+    return this._is_shown;
 };
 
 
 UI_Popover.prototype.getId = function () {
 
-	return this.id;
+    return this.id;
 };
 
 
 UI_Popover.prototype._show = function () {
 
-	this._is_shown = true;
-	this._$popover.attr('data-shown', 1);
+    this._is_shown = true;
+    this._$popover.attr('data-shown', 1);
 };
 
 
 UI_Popover.prototype._hide = function () {
 
-	this._is_shown = false;
-	this._$popover.attr('data-shown', 0);
+    this._is_shown = false;
+    this._$popover.attr('data-shown', 0);
 };
