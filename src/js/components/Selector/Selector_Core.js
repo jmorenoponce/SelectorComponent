244d3a0b2f4d6ec6129cb5382c4789e0e37c3a52
Jose Moreno <jmorenoponce@gmail.com>
2022-09-30 11:53:25 +0200

    Toggle de estado de vista extendida

--- src/js/components/Selector/Selector_Core.js
+++ src/js/components/Selector/Selector_Core.js
@@ -7,33 +7,42 @@ import {Utility} from "../../utils/Utility";
 
 export class Selector_Core {
 
+
 	static __id_counter = 0;
+
 
 	static _tpl_pointers = {
 
-		SELECTOR_BASE: 			'cmp-selector-base',
-		SELECTOR_DROPDOWN: 		'cmp-selector-dropdown',
-		SELECTOR_RESULT_ITEM: 	'cmp-selector-result-item',
-		SELECTOR_RESULT_GROUP: 	'cmp-selector-result-group',
-	}
+		SELECTOR_BASE: 			"cmp-selector-base",
+		SELECTOR_DROPDOWN: 		"cmp-selector-dropdown",
+		SELECTOR_RESULT_ITEM: 	"cmp-selector-result-item",
+		SELECTOR_RESULT_GROUP: 	"cmp-selector-result-group",
+	};
+
 
 	static _ux_pointers = {
 
-		NATIVE_FIELD_CNT: 		'.ux-selector-original-field-cnt',
-		INPUT_FIELD: 			'.ux-selector-input-field',
-		DROPDOWN_TRIGGER: 		'.ux-selector-dropdown-trigger',
-		DROPDOWN_CNT: 			'.ux-selector-dropdown-cnt',
-		SEARCH_FIELD: 			'.ux-selector-search-field',
-		VIEW_MODE_CNT:			'.ux-selector-view-mode-cnt',
-		SELECTED_CNT: 			'.ux-selector-selected-cnt',
-		RESULTS_CNT: 			'.ux-selector-results-cnt',
-		RESULT_ITEM: 			'.ux-selector-result-item'
-	}
+		NATIVE_FIELD_CNT: 		".ux-selector-original-field-cnt",
+		INPUT_FIELD: 			".ux-selector-input-field",
+		DROPDOWN_CNT: 			".ux-selector-dropdown-cnt",
+		DROPDOWN_TRIGGER: 		".ux-selector-dropdown-trigger",
+		SEARCH_FIELD: 			".ux-selector-search-field",
+		VIEW_MODE_CNT: 			".ux-selector-view-mode-cnt",
+		VIEW_MODE_UNGROUPED: 	".ux-selector-view-mode-ungrouped",
+		VIEW_MODE_GROUPED: 		".ux-selector-view-mode-grouped",
+		VIEW_MODE_EXTENDED: 	".ux-selector-view-mode-extended",
+		SELECTED_CNT: 			".ux-selector-selected-cnt",
+		RESULTS_CNT: 			".ux-selector-results-cnt",
+		RESULT_ITEM: 			".ux-selector-result-item",
+		RESULT_GROUP: 			".ux-selector-result-group"
+	};
+
 
 	static _ui_modifiers = {
 
-		DROPDOWN_OPENED: 		'ui-selector-dropdown-opened'
-	}
+		DROPDOWN_OPENED: 		"ui-selector-dropdown-opened",
+		BUTTONS_ACTIVE:			"ui-active"
+	};
 
 
 	/**
@@ -43,14 +52,14 @@ export class Selector_Core {
 	constructor($elem, config) {
 
 		this._config = {
-			category_key: '',
-			searchable_fields: [],
-			last_selected_ids: [],
-			active: true,
-			editable: true,
-			placeholder: 'Seleccionar...',
-			searching_text: 'Buscar...',
-			search_no_results: 'No se encontraron coincidencias para esta búsqueda',
+			category_key: 		"",
+			searchable_fields: 	[],
+			last_selected_ids: 	[],
+			active: 			true,
+			editable: 			true,
+			placeholder: 		"Seleccionar...",
+			searching_text: 	"Buscar...",
+			search_no_results: 	"No se encontraron coincidencias para esta búsqueda",
 
 			/**
 			 * Returns coincidence validation looking for <term> in all value keys of <searchable_fields>
@@ -64,10 +73,10 @@ export class Selector_Core {
 						return true;
 					}
 				}
-			}
+			},
 		};
 
-		this._instance_id = 'CmpSC_' + (++Selector_Core.__id_counter);
+		this._instance_id = "CmpSC_" + ++Selector_Core.__id_counter;
 
 		this._$native_field = $elem;
 		this._$native_parent_cnt = $elem.parent();
@@ -77,16 +86,19 @@ export class Selector_Core {
 			search_field: {},
 			view_mode_cnt: {},
 			selected_cnt: {},
-			results_cnt: {}
+			results_cnt: {},
 		};
 
 		this._data = [];
 
 		this._dropdown_initialized = false;
 		this._dropdown_opened = false;
-		this._list_groupable = false;
 
-		this._search_term = '';
+		this._view_mode_groupable = true;
+		this._view_mode_grouped = false;
+		this._view_mode_extended = false;
+
+		this._search_term = "";
 		this._selected_ids = [];
 
 		this.set_config(config);
@@ -118,8 +130,8 @@ export class Selector_Core {
 
 		$.extend(true, this._config, config);
 
-		if (this._config.category_key) {
-			this._list_groupable = true;
+		if (!this._config.category_key) {
+			this._view_mode_groupable = false;
 		}
 	}
 
@@ -134,14 +146,15 @@ export class Selector_Core {
 		// Only for testing
 		for (let item of this._data) {
 
-			item.avatar_initials = Utility.take_name_initials(item.first_name + ' ' + item.last_name)
-			item.activity_state = 2
-			item.user_color = 1
+			item.avatar_initials = Utility.take_name_initials( item.first_name + " " + item.last_name);
+			item.activity_state = 3;
+			item.user_color = 1;
 		}
 
 		// Only for testing
 		for (let id of this._config.last_selected_ids) {
-			this._data.find(item => item['id'] === id)['item_recent'] = 1;
+
+			this._data.find((item) => item["id"] === id)["item_recent"] = 1;
 		}
 	}
 
@@ -162,17 +175,18 @@ export class Selector_Core {
 	 */
 	set_selection(target_id) {
 
-		this._set_selected_ids(typeof (target_id) != 'object' ? [target_id] : target_id);
+		this._set_selected_ids(
+			typeof target_id != "object" ? [target_id] : target_id
+		);
 	}
 
 
 	/**
-	 *
 	 * @param target_id
 	 */
 	select_items(target_id) {
 
-		target_id = typeof (target_id) != 'object' ? [target_id] : target_id;
+		target_id = typeof target_id != "object" ? [target_id] : target_id;
 
 		if (!this._selected_ids.length) {
 
@@ -186,18 +200,17 @@ export class Selector_Core {
 
 
 	/**
-	 *
 	 * @param target_id
 	 */
 	unselect_items(target_id) {
 
-		if (typeof (target_id) == 'undefined') {
+		if (typeof target_id == "undefined") {
 
 			this._selection_remove_all();
 
 		} else {
 
-			this._selection_remove(typeof (target_id) != 'object' ? [target_id] : target_id);
+			this._selection_remove(typeof target_id != "object" ? [target_id] : target_id);
 		}
 	}
 
@@ -231,18 +244,17 @@ export class Selector_Core {
 
 
 	/**
-	 *
 	 * @private
 	 */
 	_render_init() {
 
-		let _$tmpTpl = UI_Template_Handler.$get(Selector_Core._tpl_pointers.SELECTOR_BASE, {
-			input_placeholder: this._config.placeholder,
+		let _$tmpTplBase = UI_Template_Handler.$get(Selector_Core._tpl_pointers.SELECTOR_BASE, {
+			input_placeholder: this._config.placeholder
 		});
 
-		_$tmpTpl.find(Selector_Core._ux_pointers.NATIVE_FIELD_CNT).html(this._$native_parent_cnt.html());
+		_$tmpTplBase.find(Selector_Core._ux_pointers.NATIVE_FIELD_CNT).html(this._$native_parent_cnt.html());
 
-		this._$native_parent_cnt.html('').append(_$tmpTpl);
+		this._$native_parent_cnt.html("").append(_$tmpTplBase);
 
 		this._set_events();
 		this._render_refresh();
@@ -250,17 +262,6 @@ export class Selector_Core {
 
 
 	/**
-	 *
-	 * @private
-	 */
-	_render_refresh() {
-
-		this._render_results();
-	}
-
-
-	/**
-	 *
 	 * @private
 	 */
 	_render_ensure_dropdown_init() {
@@ -272,13 +273,13 @@ export class Selector_Core {
 		this._elements.dropdown_cnt = this._$native_parent_cnt.find(Selector_Core._ux_pointers.DROPDOWN_CNT);
 		this._elements.dropdown_cnt.html(UI_Template_Handler.$get(Selector_Core._tpl_pointers.SELECTOR_DROPDOWN, {
 			search_placeholder: this._config.searching_text,
-			list_groupable: this._list_groupable ? 1 : 0
+			list_groupable: this._view_mode_groupable ? 1 : 0
 		}));
 
 		this._elements.search_field = this._elements.dropdown_cnt.find(Selector_Core._ux_pointers.SEARCH_FIELD);
 		this._elements.view_mode_cnt = this._elements.dropdown_cnt.find(Selector_Core._ux_pointers.VIEW_MODE_CNT);
 
-		this._elements.selected_cnt = this._elements.dropdown_cnt.find(Selector_Core._ux_pointers.SELECTED_CNT)
+		this._elements.selected_cnt = this._elements.dropdown_cnt.find(Selector_Core._ux_pointers.SELECTED_CNT);
 		this._elements.results_cnt = this._elements.dropdown_cnt.find(Selector_Core._ux_pointers.RESULTS_CNT);
 
 		this._dropdown_initialized = true;
@@ -286,7 +287,15 @@ export class Selector_Core {
 
 
 	/**
-	 *
+	 * @private
+	 */
+	_render_refresh() {
+
+		this._render_results();
+	}
+
+
+	/**
 	 * @private
 	 */
 	_render_results() {
@@ -295,11 +304,11 @@ export class Selector_Core {
 			return;
 		}
 
-		let $results = $('<div/>');
+		let $results = $("<div/>");
 
 		for (let item in this._data) {
 
-			if (!this._search_term || this._config.filter.apply(this, [this._search_term, this._data[item], this._config])) {
+			if (!this._search_term || this._config.filter.apply(this, [this._search_term, this._data[item], this._config,])) {
 
 				$results.append(UI_Template_Handler.$get(Selector_Core._tpl_pointers.SELECTOR_RESULT_ITEM, this._data[item]));
 			}
@@ -310,113 +319,19 @@ export class Selector_Core {
 
 
 	/**
-	 *
-	 * @param text
-	 * @private
-	 */
-	_set_search_term(text) {
-
-		this._search_term = (String(text)).toLowerCase();
-	}
-
-
-	/**
-	 *
-	 * @param ids
-	 * @private
-	 */
-	_set_selected_ids(ids) {
-
-		this._selected_ids = [...ids];
-
-		this._refresh_selection();
-	}
-
-
-	/**
-	 *
-	 * @param target_id
-	 * @private
-	 */
-	_selection_add(target_id) {
-
-		const ids = typeof (target_id) == 'object' ? target_id : [target_id];
-
-		let new_selection = [...this._selected_ids];
-
-		for (const id of ids) {
-
-			if (!new_selection.includes(id)) {
-
-				new_selection.push(id);
-			}
-		}
-
-		this._set_selected_ids(new_selection);
-	}
-
-
-	/**
-	 *
-	 * @param target_id
-	 * @private
-	 */
-	_selection_remove(target_id) {
-
-		const ids = typeof (target_id) == 'object' ? target_id : [target_id];
-		let k;
-
-		let new_selection = [];
-
-		for (const id of ids) {
-
-			if ((k = this._selected_ids.indexOf(id)) === -1) {
-
-				new_selection.push(id);
-			}
-		}
-
-		this._set_selected_ids(new_selection);
-	}
-
-
-	/**
-	 *
-	 * @private
-	 */
-	_selection_remove_all() {
-
-		this._set_selected_ids([]);
-	}
-
-
-	/**
-	 *
-	 * @private
-	 */
-	_refresh_selection() {
-
-		console.log(this._$native_field.val());
-
-		this._$native_field.val(this._selected_ids);
-	}
-
-
-	/**
-	 *
 	 * @private
 	 */
 	_set_events() {
 
-		$(Selector_Core._ux_pointers.INPUT_FIELD + ', ' + Selector_Core._ux_pointers.DROPDOWN_TRIGGER).on('click', (e) => {
+		$(Selector_Core._ux_pointers.INPUT_FIELD + ", " + Selector_Core._ux_pointers.DROPDOWN_TRIGGER).on("click", (e) => {
 			this._on_input_field_click(e);
 		});
 
-		this._$native_parent_cnt.on('keyup', Selector_Core._ux_pointers.SEARCH_FIELD, (e) => {
+		this._$native_parent_cnt.on("keyup", Selector_Core._ux_pointers.SEARCH_FIELD, (e) => {
 			this._on_search_field_keyup(e);
 		});
 
-		this._$native_parent_cnt.on('click', Selector_Core._ux_pointers.DROPDOWN_CNT, (e) => {
+		this._$native_parent_cnt.on("click", Selector_Core._ux_pointers.DROPDOWN_CNT, (e) => {
 
 			if (e.target.closest(Selector_Core._ux_pointers.RESULTS_CNT)) {
 				this._on_results_cnt_click(e.target);
@@ -429,7 +344,25 @@ export class Selector_Core {
 			}
 		});
 
-		$('body').on('click', (e) => {
+		this._$native_parent_cnt.on("click", Selector_Core._ux_pointers.VIEW_MODE_CNT, (e) => {
+
+			if (e.target.closest(Selector_Core._ux_pointers.VIEW_MODE_EXTENDED)) {
+				this._on_view_mode_extended_click();
+				return false;
+			}
+
+			if (e.target.closest(Selector_Core._ux_pointers.VIEW_MODE_UNGROUPED)) {
+				console.log("Vista por usuarios");
+				return false;
+			}
+
+			if (e.target.closest(Selector_Core._ux_pointers.VIEW_MODE_GROUPED)) {
+				console.log("Vista por equipos");
+				return false;
+			}
+		});
+
+		$("body").on("click", (e) => {
 			this._on_body_click(e);
 		});
 	}
@@ -461,13 +394,28 @@ export class Selector_Core {
 
 
 	/**
+	 * @private
+	 */
+	_on_view_mode_extended_click() {
+
+		this._view_mode_extended = !this._view_mode_extended;
+
+		let tmpCardType = this._view_mode_extended ? 'md' : 'sm';
+		$(Selector_Core._ux_pointers.RESULTS_CNT).attr('data-user-card-type', tmpCardType);
+
+		$(Selector_Core._ux_pointers.VIEW_MODE_EXTENDED).toggleClass(Selector_Core._ui_modifiers.BUTTONS_ACTIVE);
+
+		this._render_refresh();
+	}
+
+
+	/**
 	 * @param e
 	 * @private
 	 */
 	_on_results_cnt_click(item) {
 
 		let _tmpItem = item.closest(Selector_Core._ux_pointers.RESULT_ITEM);
-
 		$(_tmpItem).appendTo(this._elements.selected_cnt);
 	}
 
@@ -479,8 +427,7 @@ export class Selector_Core {
 	_on_selected_cnt_click(item) {
 
 		let _tmpItem = item.closest(Selector_Core._ux_pointers.RESULT_ITEM);
-
-		$(_tmpItem).appendTo(this._elements.results_cnt)
+		$(_tmpItem).appendTo(this._elements.results_cnt);
 	}
 
 
@@ -492,11 +439,97 @@ export class Selector_Core {
 
 		if (this._dropdown_opened) {
 
-			if (e.target !== this._$native_parent_cnt[0] && !$.contains(this._$native_parent_cnt[0], e.target)) {
-
+			if (e.target !== this._$native_parent_cnt[0] &&	!$.contains(this._$native_parent_cnt[0], e.target)) {
 				this._close();
 			}
 		}
+	}
+
+
+	/**
+	 *
+	 * @param text
+	 * @private
+	 */
+	_set_search_term(text) {
+
+		this._search_term = String(text).toLowerCase();
+	}
+
+
+	/**
+	 *
+	 * @param ids
+	 * @private
+	 */
+	_set_selected_ids(ids) {
+
+		this._selected_ids = [...ids];
+		this._refresh_selection();
+	}
+
+
+	/**
+	 *
+	 * @param target_id
+	 * @private
+	 */
+	_selection_add(target_id) {
+
+		const ids = typeof target_id == "object" ? target_id : [target_id];
+
+		let new_selection = [...this._selected_ids];
+
+		for (const id of ids) {
+			if (!new_selection.includes(id)) {
+				new_selection.push(id);
+			}
+		}
+
+		this._set_selected_ids(new_selection);
+	}
+
+
+	/**
+	 *
+	 * @param target_id
+	 * @private
+	 */
+	_selection_remove(target_id) {
+
+		const ids = typeof target_id == "object" ? target_id : [target_id];
+		let k;
+
+		let new_selection = [];
+
+		for (const id of ids) {
+			if ((k = this._selected_ids.indexOf(id)) === -1) {
+				new_selection.push(id);
+			}
+		}
+
+		this._set_selected_ids(new_selection);
+	}
+
+
+	/**
+	 *
+	 * @private
+	 */
+	_selection_remove_all() {
+
+		this._set_selected_ids([]);
+	}
+
+
+	/**
+	 *
+	 * @private
+	 */
+	_refresh_selection() {
+
+		console.log(this._$native_field.val());
+		this._$native_field.val(this._selected_ids);
 	}
 
 
