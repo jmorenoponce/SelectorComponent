a63a293d6bc6ec84413fd3a7961caad5808bbf08
Jose Moreno <jose.moreno@avanzasi.es>
2022-10-15 11:28:55 +0200

    Pruebas desde Mac

--- webpack.config.js
+++ webpack.config.js
@@ -20,28 +20,3 @@ export default {
 		}
 	}
 };
-
-
-//
-// const path = require('path');
-// const webpack = require('webpack');
-//
-// module.exports = {
-// 	mode: 'development',
-// 	entry: {index: path.resolve(__dirname, "src/js", "main.js")},
-// 	output: {
-// 		path: path.resolve(__dirname, 'dist/js'),
-// 		filename: 'main.bundle.js',
-// 	},
-// 	plugins: [
-// 		new webpack.ProvidePlugin({
-// 			$: 'jquery',
-// 			jQuery: 'jquery'
-// 		})
-// 	],
-// 	resolve: {
-// 		alias: {
-// 			handlebars: 'handlebars/dist/handlebars.min.js'
-// 		}
-// 	}
-// };
